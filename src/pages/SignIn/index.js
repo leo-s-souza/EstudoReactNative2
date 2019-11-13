2c9340ad45cb0d2075b7babe1aa85533220471a9
Leonardo Shcwarz <leo-s-souza@live.com>
2019-11-13 14:35:55 -0300

    Commit aula 1
    Configuracao inicial do projeto

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+import styles from './styles';
+
+const SignIn = () => <View style={styles.container} />;
+
+export default SignIn;
